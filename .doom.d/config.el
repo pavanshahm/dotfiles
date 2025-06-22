eb757d7859cab9932ca13d3d96cdadad5dafbd91
Ubuntu <ubuntu@ip-172-31-30-254.us-west-2.compute.internal>
2025-06-22 01:24:27 +0000

    add git auto commit mode

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -83,3 +83,4 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+(use-package git-auto-commit-mode)
--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -48,3 +48,4 @@
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;; (unpin! t)
 (package! circadian)
+(package! git-auto-commit-mode)
